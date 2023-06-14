89754babed140d65289384dd50539f0072f07ebd
ivanl <ivanloktionov@github.com>
2023-06-14 12:16:38 +0300

    tf correcting

--- variables.tf
+++ variables.tf
@@ -9,8 +9,24 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
-variable "GKE_NUM_NODES" {
-  type        = number
-  default     = 2
-  description = "GKE nodes number"
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "The GitHub owner"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifest subdirectory"
 }
